695de50c6148ab4421873a15fc38231ae17cba80
muhammad aqiilah paligie <127171345+aqiilah790@users.noreply.github.com>
2023-03-18 11:00:09 +0700

    update

---
+++ src/syekhgina16032023/Latihan65DoWhile.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sykhgna160323;
+
+/**
+ *
+ * @author user
+ */
+public class Latihan65DoWhile {
+    public static void main(String[] args) {
+        String nama = "ana"; // ganti dengan nama Anda
+        
+        int i = 0;
+        do {
+            System.out.println(nama);
+            i++;
+        } while (i < 100);
+    }
+    
+}
+
