97b009a32ee9e68834e17e33dbc295a726cb9c79
Sykhgna <syekhginaauliaputri04@gmail.com>
2023-03-30 11:39:10 +0700

    update

---
+++ src/sykhgna30032023/ValDouble.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sykhgna30032023;
+
+/**
+ *
+ * @author HP
+ */
+public class ValDouble {
+   public static void main(String[] args){
+       double valDouble = 10.12;
+    int valInt = (int)valDouble; //men-convert valDouble ke tipe int
+    double x = 10.12;
+    int y = 2;
+    int result = (int)(x/y); //hasil typecast operasi ke int
+      
+   } 
+}
